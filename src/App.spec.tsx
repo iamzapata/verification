384fdb7ecbdef2c4b61b97de1ac312bf9bc7d1a7
Andres Zapata <me@azapata.io>
2022-11-03 22:05:21 +0100

    Assert app container calls store fetch

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,13 +1,39 @@
-import { render } from '@testing-library/react'
+/* eslint-disable @typescript-eslint/await-thenable */
+import { render, renderHook, waitFor, act } from '@testing-library/react'
 import App from './App'
+import * as API from '@api'
+import { mockChecks } from './__testing__/mockData'
+import { type Store, useStore } from '@store'
 
+vi.spyOn(API, 'fetchChecks').mockImplementation(() =>
+  Promise.resolve(mockChecks)
+)
+
+let store: Store
 describe('<App />', () => {
+  beforeEach(() => {
+    if (store) {
+      store.reset()
+    }
+    store = renderHook(() => useStore()).result.current
+  })
   it('should render', () => {
-    expect(() => render(<App />)).not.toThrow()
+    expect(async () => {
+      await act(() => {
+        render(<App />)
+      })
+    }).not.toThrow()
   })
 
-  it('should match snapshot', () => {
-    const { container } = render(<App />)
-    expect(container).toMatchSnapshot()
+  it('should fetch data on render', async () => {
+    vi.spyOn(store, 'fetch')
+
+    await act(() => {
+      render(<App />)
+    })
+
+    await waitFor(() => {
+      expect(store.fetch).toHaveBeenCalledTimes(1)
+    })
   })
 })
