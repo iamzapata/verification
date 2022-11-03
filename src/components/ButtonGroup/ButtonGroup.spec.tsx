f8f63c2ba9eced3725aa21a89a2c6e5836431886
Andres Zapata <me@azapata.io>
2022-11-03 21:58:35 +0100

    Assert button group calls addAnswer

--- src/components/ButtonGroup/ButtonGroup.spec.tsx
+++ src/components/ButtonGroup/ButtonGroup.spec.tsx
@@ -1,10 +1,19 @@
-import { render } from '@testing-library/react'
+import { render, renderHook, waitFor } from '@testing-library/react'
 import { ButtonGroup } from './index'
+import { type Store, useStore } from '@store'
 import { mockChecks, renderWithUserEvent } from '@testing'
 
 const check = mockChecks[0]
 
+let store: Store
 describe('<ButtonGroup />', () => {
+  beforeEach(() => {
+    vi.resetAllMocks()
+    if (store) {
+      store.reset()
+    }
+    store = renderHook(() => useStore()).result.current
+  })
   it('renders children', () => {
     expect(() => render(<ButtonGroup check={check} inactive />)).not.toThrow()
   })
@@ -14,7 +23,9 @@ describe('<ButtonGroup />', () => {
     expect(container.firstChild).toHaveClass('ButtonGroup')
   })
 
-  it('should have an "Yes" button', async () => {
+  it('should have an "Yes" button and call "addAnswer" when clicked', async () => {
+    vi.spyOn(store, 'addAnswer')
+
     const { user, getByText } = renderWithUserEvent(
       <ButtonGroup check={check} inactive />
     )
@@ -22,5 +33,27 @@ describe('<ButtonGroup />', () => {
     const button = getByText(/yes/i)
 
     await user.click(button)
+
+    await waitFor(() => {
+      expect(store.addAnswer).toHaveBeenCalledTimes(1)
+      expect(store.addAnswer).toHaveBeenCalledWith(check, 'YES')
+    })
+  })
+
+  it('should have an "No" button and call "addAnswer" when clicked', async () => {
+    vi.spyOn(store, 'addAnswer')
+
+    const { user, getByText } = renderWithUserEvent(
+      <ButtonGroup check={check} inactive />
+    )
+
+    const button = getByText(/no/i)
+
+    await user.click(button)
+
+    await waitFor(() => {
+      expect(store.addAnswer).toHaveBeenCalledTimes(1)
+      expect(store.addAnswer).toHaveBeenCalledWith(check, 'NO')
+    })
   })
 })
