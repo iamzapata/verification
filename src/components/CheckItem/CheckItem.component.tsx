c928d8143cdd50fd2a230d5541c309e7190be7c1
Andres Zapata <me@azapata.io>
2022-11-04 23:07:03 +0100

    Add new isSelected css state to <CheckItem />

--- src/components/CheckItem/CheckItem.component.tsx
+++ src/components/CheckItem/CheckItem.component.tsx
@@ -5,13 +5,18 @@ import styles from './CheckItem.module.css'
 
 interface CheckProps {
   check: Check
+  selected: boolean
 }
-function CheckItem({ check }: CheckProps) {
+function CheckItem({ check, selected }: CheckProps) {
   const { description, inactive } = check
 
   return (
     <div
-      className={classNames(styles.CheckItem, inactive && styles.isInactive)}
+      className={classNames(
+        styles.CheckItem,
+        inactive && styles.isInactive,
+        selected && styles.isSelected
+      )}
     >
       {description}
       <ButtonGroup inactive={inactive} check={check} />
