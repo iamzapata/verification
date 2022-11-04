fe9c1298a95cdb2160b13ed387759794d787d07b
Andres Zapata <me@azapata.io>
2022-11-04 23:21:05 +0100

    Add sendResults scenario in <Submit /> spec

--- src/components/Common/Submit/Submit.component.tsx
+++ src/components/Common/Submit/Submit.component.tsx
@@ -1,18 +1,26 @@
 import { memo } from 'react'
 import { Button } from '@components'
 import { classNames } from '@utils'
+import { useStore } from '@store'
 import styles from './Submit.module.css'
 
 interface SubmitProps {
-  className: string
+  className?: string
   disabled: boolean
 }
 
 const Submit = memo(function Submit({ className, disabled }: SubmitProps) {
+  const sendResults = useStore(state => state.sendResults)
+
+  const handleSubmit = () => {
+    void sendResults()
+  }
+
   return (
     <Button
       className={classNames(styles.Submit, className)}
       disabled={disabled}
+      onClick={handleSubmit}
     >
       Submit
     </Button>
--- src/components/Common/Submit/Submit.spec.tsx
+++ src/components/Common/Submit/Submit.spec.tsx
@@ -1,9 +1,19 @@
-import { render } from '@testing-library/react'
+import { render, renderHook } from '@testing-library/react'
 import { Submit } from './Submit.component'
+import { renderWithUserEvent } from '@testing'
+import { type Store, useStore } from '@store'
+import { act } from 'react-dom/test-utils'
 
+let store: Store
 describe('<Submit />', () => {
+  beforeEach(() => {
+    if (store) {
+      store.reset()
+    }
+    store = renderHook(() => useStore()).result.current
+  })
   it('renders', () => {
-    expect(() => render(<Submit className="submit" disabled />)).not.toThrow()
+    expect(() => render(<Submit disabled />)).not.toThrow()
   })
 
   it('takes "className" prop', () => {
@@ -13,8 +23,30 @@ describe('<Submit />', () => {
   })
 
   it('should be a button', () => {
-    const { getByText } = render(<Submit className="CustomSubmit" disabled />)
+    const { getByText } = render(<Submit disabled />)
     const submit = getByText(/submit/i)
     expect(submit).toBeInstanceOf(HTMLButtonElement)
+  })
+
+  it('calls sendResults when it is not disabled', async () => {
+    vi.spyOn(store, 'sendResults')
+
+    const { user, getByText } = renderWithUserEvent(<Submit disabled={false} />)
+    const submit = getByText(/submit/i)
+
+    await user.click(submit)
+
+    expect(store.sendResults).toHaveBeenCalledTimes(1)
+  })
+
+  it('does not call sendResults when it is disabled', async () => {
+    vi.spyOn(store, 'sendResults')
+
+    const { user, getByText } = renderWithUserEvent(<Submit disabled />)
+    const submit = getByText(/submit/i)
+
+    await user.click(submit)
+
+    expect(store.sendResults).not.toHaveBeenCalled()
   })
 })
