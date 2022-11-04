c8a7c0e0d274288de4c0440e57e0031a77744776
Andres Zapata <me@azapata.io>
2022-11-04 20:21:42 +0100

    Add results submitted component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { ChecksList } from './ChecksList'
 export { CheckItem } from './CheckItem'
 export { ButtonGroup } from './ButtonGroup'
+export { ResultsSubmitted } from './ResultsSubmitted'
 export { Button, LoadingSpinner, Error, Submit } from './Common'
