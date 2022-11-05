7108df4b6ba9a205d50883d3d0ef8e78a952918e
Andres Zapata <me@azapata.io>
2022-11-05 22:21:27 +0100

    Add spec for <CheckList /> arrow move

--- src/components/ChecksList/ChecksList.component.tsx
+++ src/components/ChecksList/ChecksList.component.tsx
@@ -1,15 +1,57 @@
+import { useEffect, useRef } from 'react'
 import styles from './ChecksList.module.css'
 import { type Check } from '@store'
 import { CheckItem } from '@components'
+import { useStore, ANSWER_OPTIONS } from '@store'
 
 interface ChecksListProps {
   checks: Check[]
 }
 function ChecksList({ checks }: ChecksListProps) {
+  const setSelected = useStore(state => state.setSelected)
+  const selectedCheck = useStore(state => state.selectedCheck)
+  const index = useRef(0)
+
+  useEffect(() => {
+    function handleKeyDown(e: KeyboardEvent) {
+      const { key } = e
+
+      if ('ArrowUp' === key) {
+        if (index.current === 0) return
+
+        index.current--
+
+        setSelected(checks[index.current])
+      }
+
+      if ('ArrowDown' === key) {
+        if (index.current === checks.length - 1) return
+
+        const canSelectNext = selectedCheck?.answer === ANSWER_OPTIONS.YES
+
+        if (canSelectNext) {
+          index.current++
+          setSelected(checks[index.current])
+        }
+      }
+    }
+
+    document.addEventListener('keydown', handleKeyDown)
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+    // Don't feel great about the stringifying here, but it works for now
+  }, [JSON.stringify(checks), selectedCheck?.id])
+
   return (
     <section className={styles.ChecksList}>
       {checks.map(check => (
-        <CheckItem key={check.id} check={check} />
+        <CheckItem
+          key={check.id}
+          check={check}
+          selected={selectedCheck?.id === check.id}
+        />
       ))}
     </section>
   )
--- src/components/ChecksList/ChecksList.spec.tsx
+++ src/components/ChecksList/ChecksList.spec.tsx
@@ -1,8 +1,21 @@
-import { render } from '@testing-library/react'
+import { render, renderHook, act } from '@testing-library/react'
+import { renderWithUserEvent } from '@testing'
 import { ChecksList } from './ChecksList.component'
 import { mockChecks } from '@testing'
+import { type Store, useStore } from '@store'
 
+const checks = mockChecks
+
+let getState: () => Store
 describe('<ChecksList />', () => {
+  beforeEach(() => {
+    if (getState) {
+      getState().reset()
+    }
+    const { result } = renderHook(() => useStore())
+    getState = () => result.current
+  })
+
   it('renders', () => {
     expect(() => render(<ChecksList checks={[]} />)).not.toThrow()
   })
@@ -13,7 +26,7 @@ describe('<ChecksList />', () => {
   })
 
   it('it should render checks using the <CheckItem> component', () => {
-    const { container } = render(<ChecksList checks={mockChecks} />)
+    const { container } = render(<ChecksList checks={checks} />)
     const children = container?.firstChild?.childNodes ?? []
 
     expect(children?.length).toEqual(4)
@@ -21,5 +34,52 @@ describe('<ChecksList />', () => {
     Array.from(children).forEach(child =>
       expect(child).toHaveClass('CheckItem')
     )
+  })
+
+  it('should call "setSelected" with next check when "ArrowDown" is triggered', async () => {
+    vi.spyOn(getState(), 'setSelected')
+
+    const { addAnswer, getChecks } = getState()
+
+    await act(() => {
+      return getChecks()
+    })
+
+    const { checks } = getState()
+
+    act(() => addAnswer(checks[0], 'YES'))
+
+    const { user } = renderWithUserEvent(<ChecksList checks={checks} />)
+
+    await act(() => user.keyboard('{ArrowDown}'))
+
+    const { setSelected, selectedCheck } = getState()
+
+    expect(setSelected).toHaveBeenCalledTimes(1)
+    expect(setSelected).toHaveBeenCalledWith(checks[1])
+    expect(selectedCheck).toEqual(checks[1])
+  })
+
+  it('should call "setSelected" with previous check when "ArrowUp" is triggered', async () => {
+    vi.spyOn(getState(), 'setSelected')
+
+    const { addAnswer, getChecks, setSelected } = getState()
+
+    await act(() => {
+      return getChecks()
+    })
+
+    const { checks } = getState()
+
+    const { user } = renderWithUserEvent(<ChecksList checks={checks} />)
+
+    let selectedCheck
+    act(() => addAnswer(checks[0], 'YES'))
+    await act(() => user.keyboard('{ArrowDown}'))
+    selectedCheck = getState().selectedCheck
+    expect(selectedCheck).toEqual(checks[1])
+    await act(() => user.keyboard('{ArrowUp}'))
+    selectedCheck = getState().selectedCheck
+    expect(selectedCheck).toEqual(checks[0])
   })
 })
